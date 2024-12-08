a5aab9c77121a04433b24c39c160c8982a158a6e
Steven Wilkin <stevenwilkin@gmail.com>
2024-12-08 16:21:07 +0700

    6 figure bitcoin

--- main.go
+++ main.go
@@ -18,6 +18,6 @@ func main() {
 		}
 
 		date := time.UnixMilli(t.Timestamp).Format(time.DateOnly)
-		fmt.Printf("%s\t%9.6f\t%.2f\t%7.2f\n", date, t.InterestPl, t.Price, t.InterestPl*t.Price)
+		fmt.Printf("%s\t%9.6f\t%9.2f\t%7.2f\n", date, t.InterestPl, t.Price, t.InterestPl*t.Price)
 	}
 }
